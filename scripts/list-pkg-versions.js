df1ed0bf6e70669e4e4d18dc3cf296f87286edf0
Justin Zhu <vac872089248@gmail.com>
2023-07-21 14:27:21 +0800

    chore: add list-pkg-versions script

---
+++ scripts/list-pkg-versions.js
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+const DIRS = [path.resolve(__dirname, '../packages/adapters'), path.resolve(__dirname, '../packages/react')];
+const pkgVersions = [];
+DIRS.forEach((dir) => {
+    const subDirs = fs.readdirSync(dir);
+    subDirs.forEach((pkg) => {
+        try {
+            const { name, version } = JSON.parse(fs.readFileSync(path.resolve(dir, pkg, 'package.json')));
+            pkgVersions.push({
+                name,
+                version,
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    });
+});
+
+pkgVersions.forEach(({ name, version }) => {
+    const oldVersion = execSync(`npm view ${name} version`);
+    console.log(`${name}: ${oldVersion.toString().trim()} -> ${version}`);
+});
+
+console.log('Tag Content: ');
+pkgVersions.forEach(({ name, version }) => {
+    console.log(`- [${name}@${version}](https://www.npmjs.com/package/${name})`);
+});
