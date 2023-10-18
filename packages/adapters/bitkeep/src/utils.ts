6cf7c2384f3dda74c345666117135e6e166c9311
kean <kean.yang@bitget.comkean.yang@bitget.com>
2023-10-18 16:33:06 +0800

    feat: upgraded bitKeepWallet

--- packages/adapters/bitkeep/src/utils.ts
+++ packages/adapters/bitkeep/src/utils.ts
@@ -1,11 +1,11 @@
 import { isInMobileBrowser } from '@tronweb3/tronwallet-abstract-adapter';
 
-export function supportBitKeep() {
+export function supportBitgetWallet() {
     return !!window.tronLink && (window as any).isBitKeep;
 }
 
-export function openBitKeep() {
-    if (isInMobileBrowser() && !supportBitKeep()) {
+export function openBitgetWallet() {
+    if (isInMobileBrowser() && !supportBitgetWallet()) {
         const { origin, pathname, search, hash } = window.location;
         const url = origin + pathname + search + hash;
         location.href = `https://bkcode.vip?action=dapp&url=${encodeURIComponent(url)}`;
