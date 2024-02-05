dd154867e6c35e4b5a88e90c8fb1501931cfb69b
Justin Zhu <vac872089248@gmail.com>
2024-02-05 14:29:13 +0800

    feat(okxwallet): update the deeplink url

--- packages/adapters/okxwallet/src/utils.ts
+++ packages/adapters/okxwallet/src/utils.ts
@@ -4,9 +4,16 @@ export function supportOkxWallet() {
     return !!(window.okxwallet && window.okxwallet.tronLink);
 }
 
+export const isOKApp = /OKApp/i.test(navigator.userAgent);
+
 export function openOkxWallet() {
-    if (!supportOkxWallet() && isInMobileBrowser()) {
-        window.location.href = `okx://wallet/dapp/details?dappUrl=${window.location.href}`;
+    alert((navigator as any).userAgentData);
+    if (!isOKApp && isInMobileBrowser()) {
+        const encodedUrl =
+            'https://www.okx.com/download?deeplink=' +
+            encodeURIComponent('okx://wallet/dapp/url?dappUrl=' + encodeURIComponent(window.location.href));
+        alert('deeplink');
+        window.location.href = 'okx://wallet/dapp/url?dappUrl=' + encodeURIComponent(window.location.href);
         return true;
     }
     return false;
